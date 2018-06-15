aca954b7224b79674025ff9da15f86c8e5f3e29f
ratmadja <ratmadja@gmail.com>
2018-06-14 19:05:28 -0700

    cp9-jquerySongList:done

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -3,8 +3,8 @@
 //Insert album.data into the DOM #album-title element
 $('#album-title').text(album.title);
 
-//Use .attr() method to add album.albumArtUrl to the
-//src attribute of the img#album-cover-art DOM element.
+//Use .attr() method to add album.albumArtUrl (from album-info.js) to the
+//src attribute of the img#album-cover-art DOM element (from index.html).
 $('img#album-cover-art').attr('src',album.albumArtUrl);
 
 //artist Info
